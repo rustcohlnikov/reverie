42088526ef7ad32fd8428bd7d76dbc8dc205e36d
vincenx <freakypinky777@gmail.com>
2018-10-15 21:24:10 +0600

    Add dist folder

--- src/entry/production.js
+++ src/entry/production.js
@@ -1,5 +1,4 @@
 import ReverieDatepicker from '../components/Datepicker'
-import '../stylesheets/main.scss'
 
 ReverieDatepicker.install = function (Vue) {
   Vue.component(ReverieDatepicker.name, ReverieDatepicker)
